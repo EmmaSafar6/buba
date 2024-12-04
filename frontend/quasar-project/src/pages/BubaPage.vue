f9b5c0fb9604eb72611bc21c77c6ee162195ebbb
Emma Safar <safaremma6@gmail.com>
2024-12-04 17:21:14 +0100

    Poruka commit

---
+++ frontend/quasar-project/src/pages/BubaPage.vue
@@ -0,0 +1,12 @@
+<template>
+  <q-page padding>
+    <!-- content -->
+    <q-btn square color="purple" glossy icon="local_grocery_store" />
+  </q-page>
+</template>
+
+<script>
+export default {
+  // name: 'PageName',
+}
+</script>
